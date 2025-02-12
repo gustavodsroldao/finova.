11844f695ab4bbc3839bf7f9de9d51cdd1b6446e
gustavodsroldao <gustavodsroldao@gmail.com>
2025-02-12 20:28:09 -0300

    fix: emergencial

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "my-app/_components/ui/button"
+
+const Home = () => {
+  return <Button>Hello World</Button>;
+}
+
+export default Home
--- my-app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button"
-
-const Home = () => {
-  return <Button>Hello World</Button>;
-}
-
-export default Home
