7163c6987380200cec9f1f33a5b4467f22d8e4cc
gustavodsroldao <gustavodsroldao@gmail.com>
2025-02-12 20:44:29 -0300

    feat: Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,21 +4,21 @@ import { LogInIcon } from "lucide-react";
 
 const LoginPage = async () => {
     return (
-        <div className="grid h-full grid-cols-2">
+        <div className="grid h-full">
             {/* CENTER */}
             <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
                 <Image
                     src="/logo.svg"
                     width={173}
                     height={39}
-                    alt="Finance AI"
+                    alt="finova."
                     className="mb-8"
                 />
                 <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
                 <p className="mb-8 text-muted-foreground">
-                    A Finance AI é uma plataforma de gestão financeira que utiliza IA para
-                    monitorar suas movimentações, e oferecer insights personalizados,
-                    facilitando o controle do seu orçamento.
+                    O poder de controlar suas finanças, na palma da mão.
+                    Facilitando o controle do seu dinheiro de um jeito
+                    simples e inteligente.
                 </p>
                     <Button variant="outline">
                         <LogInIcon className="mr-2" />
